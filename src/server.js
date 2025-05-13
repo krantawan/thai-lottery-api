c1118d7bda32588b4740e2e3b9f0f4450a38d488
krantawan <krantawan@outlook.com>
2025-05-13 16:08:30 +0700

    Edit Server.js

--- src/server.js
+++ src/server.js
@@ -9,12 +9,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../Public')));
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '../Public')));
 
 const allowedOrigins = [
   /^https:\/\/(www\.)?yourdomain\.com$/,
